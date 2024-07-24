8697b3302d7c866d99e39a6e8a83bf168c2bcb1e
Alexandros Christopoulos <alex1951132@gmail.com>
2024-07-24 17:48:40 +0300

    identicon implementation (draft)

--- identicon.js
+++ identicon.js
@@ -1,8 +1,10 @@
+const { createCanvas, loadImage } = require("canvas");
+const fs = require("fs");
 const { createHash } = require("crypto");
 
 const email = process.env.ACCOUNT;
 
-const hash = createHash("sha256").update(email).digest("hex");
+const hash = createHash("md5").update(email).digest("hex");
 
 const hexColor = hash.slice(0, 6);
 
@@ -21,10 +23,29 @@ const thirdColumn = binaryString.slice(10, 15);
 const fourthColumn = secondColumn.split("").reverse().join("");
 const fifthColumn = firstColumn.split("").reverse().join("");
 
-console.log([
-  firstColumn,
-  secondColumn,
-  thirdColumn,
-  fourthColumn,
-  fifthColumn,
-]);
+const pixels = [
+  firstColumn.split(""),
+  secondColumn.split(""),
+  thirdColumn.split(""),
+  fourthColumn.split(""),
+  fifthColumn.split(""),
+];
+
+const height = 5;
+const width = 5;
+const canvas = createCanvas(height, width);
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = `#${hexColor}`;
+
+pixels.forEach((row, xIndex) =>
+  row.forEach((toFill, yIndex) => {
+    if (toFill === "1") {
+      ctx.fillStyle = `#${hexColor}`;
+      ctx.fillRect(xIndex + 1, yIndex + 1, 1, 1);
+    }
+  })
+);
+
+const buffer = canvas.toBuffer("image/png");
+fs.writeFileSync("./identicon.png", buffer);
